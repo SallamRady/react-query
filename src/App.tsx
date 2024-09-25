c8ef4e2c5c9b280743bf7f792259d3bd8766570d
Sallam Rady <sallamrady@gmail.com>
2024-09-26 00:43:47 +0300

    implement main configration of react query library

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,24 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SuperHerosPage from "./pages/SuperHerosPage";
 import RQSuperHeroPage from "./pages/RQSuperHeroPage";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 function App() {
+  // ** declare and define component state and variables
+  const queryClient = new QueryClient();
+
+  // ** return component ui
   return (
-    <div>
-      <MainNavbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/super-heroes" element={<SuperHerosPage />} />
-        <Route path="/rq-super-heroes" element={<RQSuperHeroPage />} />
-      </Routes>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <MainNavbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/super-heroes" element={<SuperHerosPage />} />
+          <Route path="/rq-super-heroes" element={<RQSuperHeroPage />} />
+        </Routes>
+      </div>
+    </QueryClientProvider>
   );
 }
 
